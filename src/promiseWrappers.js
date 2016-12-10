7b9e21f7a322f041c0530251e1401a23f505a40b
evegreen <r.baskov@mail.ru>
2016-12-11 03:31:02 +0700

    promise wrappers refactoring almost done

--- src/promiseWrappers.js
+++ src/promiseWrappers.js
@@ -40,18 +40,15 @@ exports.promisifyWrapper1res = function (func, selector) {
 
 // this wrapper cannot handle function with many results,
 // cause promise can pass only one of them to resolve function
-exports.unifyWrapper = function (func, ...args) {
-  let callback = function (err, result) {
-    if (err) {
-      return reject(err);
-    }
-
-    return resolve(result);
-  }
-
-  args.push(callback);
-
+exports.unifyWrapper = function (func, ...funcArgs) {
   return new Promise((resolve, reject) => {
-    func.apply(args);
+    debugger;
+    func(...funcArgs, (err, result) => {
+      if (err) {
+        return reject(err);
+      }
+
+      return resolve(result);
+    });
   });
 };
--- tests/unit/promiseWrappersTest.js
+++ tests/unit/promiseWrappersTest.js
@@ -7,11 +7,6 @@ var funcWithoutArgs = function (cb) {
   return cb(null);
 };
 
-var funcWithArg = function (arg, cb) {
-  assert.equal(arg, 'example arg');
-  return cb(null);
-};
-
 var funcWith2Args = function (arg1, arg2, cb) {
   assert.equal(arg1, 'example arg1');
   assert.equal(arg2, 'example arg2');
@@ -22,46 +17,112 @@ var funcWithResult = function (cb) {
   return cb(null, 'example result');
 };
 
+var funcWithError = function (cb) {
+  return cb('example error');
+}
+
 // mocha done callback will be stored in global,
 // cause we need test arguments directly in this tests
 var resolveFunc = function (err) {
   assert.isNull(err);
-  global.simpleCallbackDone();
 };
 
 var rejectFunc = function (err) {
   assert.equal(err, 'example error');
-  global.errorCallbackDone();
 };
 
 var resolveWithResult = function (err, res) {
   assert.isNull(err);
   assert.equal(res, 'example result');
-  global.callbackWithResultDone();
 };
 
 describe('promise wrappers', () => {
   it('works without argumets', done => {
-    global.simpleCallbackDone = done;
-    let promised = promiseWrapper(funcWithoutArgs);
-    promised().then(resolveFunc);
-    global.simpleCallbackDone = null;
-  });
-/*
-  it('works with 1 argument', done => {
+    let funcCalled = false;
+    let funcWithoutArgs = function (cb) {
+      funcCalled = true;
+      return cb(null);
+    };
 
+    let promisedFuncWithoutArgs = function () {
+      return promiseWrapper(funcWithoutArgs);
+    };
+
+    promisedFuncWithoutArgs()
+    .then(() => {
+      assert.isTrue(funcCalled);
+      done();
+    });
+  });
+
+  it('works with 1 argument', done => {
+    let funcCalled = false;
+    let funcWithArg = function (arg, cb) {
+      assert.equal(arg, 'example arg');
+      funcCalled = true;
+      return cb(null);
+    };
+
+    let promisedFuncWithArg = function (arg) {
+      return promiseWrapper(funcWithArg, arg);
+    };
+
+    promisedFuncWithArg('example arg')
+    .then(() => {
+      assert.isTrue(funcCalled);
+      done();
+    });
   });
 
   it('works with 2 arguments', done => {
+    let funcCalled = false;
+    let funcWith2Args = function (arg1, arg2, cb) {
+      assert.equal(arg1, 'example arg1');
+      assert.equal(arg2, 'example arg2');
+      funcCalled = true;
+      return cb(null);
+    };
 
+    let promisedFuncWith2Args = function (arg1, arg2) {
+      return promiseWrapper(funcWith2Args, arg1, arg2);
+    };
+
+    promisedFuncWith2Args('example arg1', 'example arg2')
+    .then(() => {
+      assert.isTrue(funcCalled);
+      done();
+    });
   });
 
   it('works with result', done => {
+    let funcWithResult = function (cb) {
+      return cb(null, 'example result');
+    };
 
+    let promisedFuncWithResult = function () {
+      return promiseWrapper(funcWithResult);
+    }
+
+    promisedFuncWithResult()
+    .then(result => {
+      assert.equal(result, 'example result');
+      done();
+    });
   });
 
   it('works with error', done => {
+    let funcThatThrows = function (cb) {
+      return cb('example error');
+    };
 
+    let promisedFuncThatThrows = function () {
+      return promiseWrapper(funcThatThrows);
+    }
+
+    promisedFuncThatThrows()
+    .catch(err => {
+      assert.equal(err, 'example error');
+      done();
+    });
   });
-*/
 });
